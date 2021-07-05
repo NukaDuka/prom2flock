73ed5bddca59e152015a16366563dcf4b807e264
Kartik Gokte <ksgokte@gmail.com>
2021-07-05 13:47:07 +0530

    Fixed python version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email = "ksgokte@gmail.com",
     description = "A web service that processes and routes Alertmanager alerts to any Flock channel",
     url = "https://github.com/NukaDuka/prom2flock",
-    python_requires="==3.8",
+    python_requires="==3.8.9",
     package_dir={"": "src"},
     packages = ['prom2flock'],
     install_requires = [
