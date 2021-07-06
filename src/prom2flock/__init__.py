c9ab5b08da3ab2c2723c07d2ebc504e99cc2837f
Kartik Gokte <ksgokte@gmail.com>
2021-07-06 12:55:39 +0530

    Fixed bug in os environment import

--- src/prom2flock/__init__.py
+++ src/prom2flock/__init__.py
@@ -5,10 +5,11 @@ import os
 
 if __name__ == "__main__":
     logging.basicConfig(format='%(asctime)s [%(funcName)s (%(name)s)]  %(levelname)s: %(message)s', level=logging.DEBUG)
-    # check if gunicorn exists
-    output = subprocess.run(['gunicorn', '-h'], shell=True)
+    # Import environment from os
+    os_env = os.environ.copy()
+    # Check if gunicorn exists
+    output = subprocess.run(['gunicorn', '-h'], shell=True, env=os_env)
     if output.returncode != 0:
         logging.error('Gunicorn cannot be found in $PATH, exiting')
         sys.exit(1)
-    os_env = os.environ.copy()
     output = subprocess.run(['gunicorn', '-c', './gunicorn.conf.py', 'wsgi:app'], shell=True, env=os_env)
