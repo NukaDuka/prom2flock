50cd16bca9b3f2627c4fd05f744b59d9bcd80f54
Kartik Gokte <ksgokte@gmail.com>
2021-08-06 14:41:18 +0530

    Removed dependency on subprocess
    prom2flock now uses gunicorn.app.wsgiapp to launch
    
    Added .editorconfig file
    
    Bumped up version to 0.4.0

--- src/prom2flock/__init__.py
+++ src/prom2flock/__init__.py
@@ -1,12 +1,14 @@
-import subprocess
 import logging
+import re
 import sys
 import os
 import argparse
-from . import gunicorn_conf
+from gunicorn.app.wsgiapp import run
+import gunicorn_conf
 
 def main():
     # Get and parse command line arguments
+    print(gunicorn_conf)
     argparser = argparse.ArgumentParser(description='A web service that processes and routes alertmanager alerts to Flock')
     argparser.add_argument('-c', '--config', default='/etc/prom2flock/config.yaml', help='prom2flock config file location')
     args = argparser.parse_args()
@@ -14,14 +16,17 @@ def main():
     config_file_location = args.config
 
     logging.basicConfig(format='%(asctime)s [%(funcName)s (%(name)s)]  %(levelname)s: %(message)s', level=logging.DEBUG)
-    # Import environment from os
-    os_env = os.environ.copy()
-    os_env['CONFIG_FILE_LOCATION'] = config_file_location
-    # Check if gunicorn exists
-    output = subprocess.run(['gunicorn', '-v'], env=os_env)
-    if output.returncode != 0:
-        logging.error('Gunicorn cannot be found in $PATH, exiting')
-        sys.exit(1)
-    output = subprocess.run(['gunicorn', '-c', gunicorn_conf.__file__, 'wsgi:app'], env=os_env, cwd=os.path.dirname(gunicorn_conf.__file__))
+    # Add prom2flock config location to environment variables
+    os.environ['CONFIG_FILE_LOCATION'] = config_file_location
+    sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
+    
+    os.chdir(os.path.dirname(gunicorn_conf.__file__))
+    
+    # This argv will be passed along to gunicorn
+    sys.argv = [sys.argv[0], '-c', gunicorn_conf.__file__, 'wsgi:app']
+    
+    # Run gunicorn and pass along exit code
+    sys.exit(run())
+    
 if __name__ == "__main__":
     main()
