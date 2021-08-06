9fdf25d67a30d75150f8870117bcefc7a7ff4d72
Kartik Gokte <ksgokte@gmail.com>
2021-08-06 17:53:50 +0530

    messing around with imports

--- src/prom2flock/main.py
+++ src/prom2flock/main.py
@@ -6,13 +6,13 @@ import argparse
 
 from gunicorn.app.wsgiapp import run
 
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-print(os.getcwd())	
+#os.chdir(os.path.dirname(os.path.abspath(__file__)))
+#print(os.getcwd())
 
-from gunicorn_conf import __file__ as gunicorn_conf_file
+from prom2flock import gunicorn_conf
 
 def main():
-    
+
     argparser = argparse.ArgumentParser(description='A web service that processes and routes alertmanager alerts to Flock')
     argparser.add_argument('-c', '--config', default='/etc/prom2flock/config.yaml', help='prom2flock config file location')
     args = argparser.parse_args()
@@ -23,13 +23,13 @@ def main():
     # Add prom2flock config location to environment variables
     os.environ['CONFIG_FILE_LOCATION'] = config_file_location
     sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
-    
-    
+
+
     # This argv will be passed along to gunicorn
-    sys.argv = [sys.argv[0], '-c', gunicorn_conf_file, 'wsgi:app']
-    
+    sys.argv = [sys.argv[0], '-c', gunicorn_conf.__file__, 'wsgi:app']
+
     # Run gunicorn and pass along exit code
     sys.exit(run())
-    
+
 if __name__ == "__main__":
     main()
